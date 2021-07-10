1f94eea8bf70d36abe70ac1a2cdd483206fe3562
suraj-pawar <35092126+suraj-pawar@users.noreply.github.com>
2021-07-10 18:32:49 +0530

    Added description for @Configuration and @Bean.

--- javaconfig/src/main/java/com/java/config/HelloWorldConfig.java
+++ javaconfig/src/main/java/com/java/config/HelloWorldConfig.java
@@ -4,9 +4,15 @@ import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+// Configuration annotation indicates that this class defines one or more @Bean method 
+// which will be processed by the spring container to generate the Bean definitions and
+// service at runtime.
 @Configuration
 public class HelloWorldConfig {
 
+
+// @Bean annotation indicates a particular beans definition which will be processed 
+// at runtime.
 	@Bean(name="helloWorld")
 	public HelloWorld getHelloWorld() {
 		return new HelloWorld();
